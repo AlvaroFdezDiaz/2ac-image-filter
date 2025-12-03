77237b6d4310e682cdb49471d4bbb46832948a1e
Álvaro Fernández <70291440+AlvaroFdezDiaz@users.noreply.github.com>
2025-12-03 15:26:33 +0100

    Formalización de formato y revisión de comentarios

--- Single-Thread/main.cpp
+++ Single-Thread/main.cpp
@@ -11,8 +11,8 @@
 
 using namespace cimg_library;
 
-// Data type for image components
-typedef double data_t; // Cambiado a tipo de dato double
+// Tipo de dato double
+typedef double data_t;
 //Imagen original
 const char* SOURCE_IMG      = "../Photos/normal/bailarina.bmp";
 //Imagen con degradado
@@ -24,18 +24,22 @@ int saturationControl(int x, int y);
 
 // Filter argument data type
 typedef struct {
-	data_t *pRsrc; // Pointers to the R, G and B components
+	// Punteros a las componentes R, G y B de la imagen source
+	data_t *pRsrc;
 	data_t *pGsrc;
 	data_t *pBsrc;
+
+	// Punteros a las componentes R, G y B de la imagen destino
 	data_t *pRdst;
 	data_t *pGdst;
 	data_t *pBdst;
-	uint pixelCount; // Size of the image in pixels
+
+	uint pixelCount; // Número de píxeles de la imagen
 } filter_args_t;
 
 /***********************************************
  * 
- * Algorithm. Image filter.
+ * Algoritmo. Filtro de imagen.
  * Problema 12 - Darken
  * Oscurece la imagen original en función de los valores de la segunda imagen
  * *********************************************/
@@ -63,68 +67,67 @@ int saturationControl(int x, int y) {
 }
 
 int main() {
-	// Open file and object initialization
+	// Abrir imagen source con CImg
 	CImg<data_t> srcImage(SOURCE_IMG);
 	CImg<data_t> srcImage2(SOURCE_IMG2);
 
 	filter_args_t filter_args;
 	filter_args_t filter_args2;
-	data_t *pDstImage; // Pointer to the new image pixels
+	data_t *pDstImage; // Puntero a los píxeles de la imagen destino
 
-	srcImage.display(); // Displays the source image
-	srcImage2.display();
-	uint width = srcImage.width();// Getting information from the source image
-	uint height = srcImage.height();	
-	uint nComp = srcImage.spectrum();// source image number of components
-	         // Common values for spectrum (number of image components):
-				//  B&W images = 1
-				//	Normal color images = 3 (RGB)
-				//  Special color images = 4 (RGB and alpha/transparency channel)
-	//Segunda imagen
+	// Mostrar imágenes source
+	srcImage.display();
+	srcImage2.display(); 
+
+	uint width = srcImage.width(); // Ancho de la imagen source
+	uint height = srcImage.height(); // Alto de la imagen source
+	uint nComp = srcImage.spectrum(); // Número de componentes de cada píxel de la imagen
+
+	// Lo mismo con la segunda imagen source
 	uint width2 = srcImage2.width();
 	uint height2 = srcImage2.height();
 	uint nComp2 = srcImage2.spectrum();
 
-	//Comprobación sobre el tamaño de las imágenes
+	// Comprobamos que el tamaño de las dos imágenes es el mismo
 	if (width != width2 || height != height2 || nComp != nComp2) {
 		printf("Error: Not same size images");
 		return(-1);
 	}
 
-	// Calculating image size in pixels
+	// Cálculo del número de píxeles de la imagen
 	filter_args.pixelCount = width * height;
 	filter_args2.pixelCount = width2 * height2;
 	
-	// Allocate memory space for destination image components
+	// Reserva de memoria para las componentes de la imagen destino
 	pDstImage = (data_t *) malloc (filter_args.pixelCount * nComp * sizeof(data_t));
 	if (pDstImage == NULL) {
 		perror("Allocating destination image");
 		exit(-2);
 	}
 
-	// Pointers to the componet arrays of the source image
-	filter_args.pRsrc = srcImage.data(); // pRcomp points to the R component array
-	filter_args.pGsrc = filter_args.pRsrc + filter_args.pixelCount; // pGcomp points to the G component array
-	filter_args.pBsrc = filter_args.pGsrc + filter_args.pixelCount; // pBcomp points to B component array
+	// Punteros a las posiciones de las componentes R, G y B de la primera imagen source
+	filter_args.pRsrc = srcImage.data();
+	filter_args.pGsrc = filter_args.pRsrc + filter_args.pixelCount;
+	filter_args.pBsrc = filter_args.pGsrc + filter_args.pixelCount;
 	
-	//Segunda imagen
+	// Punteros a las posiciones de las componentes R, G y B de la segunda imagen source
 	filter_args2.pRsrc = srcImage2.data();
 	filter_args2.pGsrc = filter_args2.pRsrc + filter_args2.pixelCount;
 	filter_args2.pBsrc = filter_args2.pGsrc + filter_args2.pixelCount;
 
-	// Pointers to the RGB arrays of the destination image
+	// Punteros a las posiciones de las componentes R, G y B de la imagen destino
 	filter_args.pRdst = pDstImage;
 	filter_args.pGdst = filter_args.pRdst + filter_args.pixelCount;
 	filter_args.pBdst = filter_args.pGdst + filter_args.pixelCount;
-	// Variables para el tiempo
+
+	// Variables para el cálculo del tiempo de ejecución
 	struct timespec tStart, tEnd;
     double dElapsedTimeS;
-
 
 	clock_gettime(CLOCK_REALTIME, &tStart);  // Inicio del contador de tiempo de ejecución
 
 	/************************************************
-	 * Algorithm.
+	 * Algoritmo.
 	 */
 	filter(filter_args,filter_args2);
 
@@ -133,21 +136,19 @@ int main() {
 	//Cálculo del tiempo transcurrido
 	dElapsedTimeS = (tEnd.tv_sec - tStart.tv_sec) + (tEnd.tv_nsec - tStart.tv_nsec) / 1e+9;
 		
-	// Create a new image object with the calculated pixels
-	// In case of normal color images use nComp=3,
-	// In case of B/W images use nComp=1.
+	// Creación de nueva imagen con CImg para almacenar la imagen destino
 	CImg<data_t> dstImage(pDstImage, width, height, 1, nComp);
 
-	// Store destination image in disk
+	// Guardar imagen destino en disco
 	dstImage.save(DESTINATION_IMG); 
 
-	// Display destination image
+	// Mostrar imagen destino
 	dstImage.display();
 	
-	//Imprimir tiempo total de procesamiento
+	// Imprimir tiempo total de procesamiento
 	printf("Tiempo total de procesamiento: %.6f s\n", dElapsedTimeS);
 
-	// Free memory
+	// Liberar memoria
 	free(pDstImage);
 
 	return 0;
